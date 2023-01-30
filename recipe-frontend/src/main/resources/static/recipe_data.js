42968a40042471c6f26e4cb2ca3f8f6704b88c09
Josh Archer <jarcher@greenriver.edu>
2023-01-30 13:49:18 -0800

    Added front-end files for an example with fetch().

--- recipe-frontend/src/main/resources/static/recipe_data.js
+++ recipe-frontend/src/main/resources/static/recipe_data.js
@@ -23,6 +23,36 @@ function loadRecipes()
 
 function showRecipes(recipes)
 {
+    //get our list
+    let list = document.getElementById("recipes");
 
+    //loop over all our recipes
+    for (let i = 0; i < recipes.length; i++)
+    {
+        //add each recipe to the list
+        let recipe = recipes[i];
+
+        //create element using a function on document
+        let li = document.createElement("li"); //<li></li>
+        li.innerHTML = recipe.name; //<li>Tacos</li>
+
+        //add spans
+        let servingSpan = document.createElement("span");
+        let timeSpan = document.createElement("span");
+
+        servingSpan.className = "servings";
+        timeSpan.className = "cook-time";
+
+        servingSpan.innerHTML = recipe.servings + " servings";
+        timeSpan.innerHTML = recipe.cookTime + " minutes";
+
+        li.appendChild(servingSpan);
+        li.appendChild(timeSpan);
+
+        //attach the li to the DOM
+        list.appendChild(li);
+
+        //<li>Nigerian Rice <span>6 servings</span><span>30 minutes</span></li>
+    }
 }
 
